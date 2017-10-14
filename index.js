73683cf2dcac869b71e88b05d0dc6100d585473c
raymondborkowski <rborkows@umich.edu>
2017-10-13 22:42:53 -0400

    RB: Loop through different values

--- index.js
+++ index.js
@@ -1,5 +1,33 @@
-const forLoop = function forLoop(callback){
-    callback();
+const isObject = function isObject(whoAmI){
+    return (!!whoAmI) && (whoAmI.constructor === Object);
 };
 
-module.exports = forLoop;
+const isArray = function isArray(whoAmI){
+    return (!!whoAmI) && (whoAmI.constructor === Array);
+};
+
+const isNumber = function isNumber(whoAmI){
+    return !isNaN(whoAmI);
+};
+
+const fourLoop = function fourLoop(loopMe, callback){
+    if (isObject(loopMe)){
+        Object.keys(loopMe).forEach((k) => {
+            callback(loopMe[k], k);
+        });
+    } else if (isArray(loopMe)) {
+        for (const value of loopMe) {
+            callback(value);
+        }
+    } else if (isNumber(loopMe)) {
+        for (let i = 0; i < loopMe; i++) {
+            callback(i);
+        }
+    } else if (isMap(loopMe)) {
+        for (const [k, v] of loopMe) {
+            callback(v, k);
+        }
+    }
+};
+
+module.exports = fourLoop;
