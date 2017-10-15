6114a0b0e1091b63646f484d2168c2f3e3a02313
raymondborkowski <rborkows@umich.edu>
2017-10-15 00:43:18 -0400

    RB: Allow ++ operator

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "extends": "airbnb-base",
     "rules": {
         "indent": ["error", 4],
-        "space-before-blocks": "off"
+        "space-before-blocks": "off",
+        "no-plusplus": "off"
     },
     "globals": {
         "describe": true,
